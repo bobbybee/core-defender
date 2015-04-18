a043461f2acf71c7f2ee184eb6929f2e4d9f2232
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-04-18 14:50:19 -0400

    Missles by array

--- donutdefender.c
+++ donutdefender.c
@@ -1,32 +1,40 @@
+int* userspace = 131072;
+
 structptr(Sprite) core;
 
-structptr(Sprite) missle1;
-int missle1Active = 0;
+structptr(Sprite) missles;
+int* misslesActive = userspace + 0;
 
 int timeout = 100;
 
 void shootMissleSprite(structptr(Sprite) missle) {
-  missle->x = -2;
-  missle->y = -2;
+  missle = newSprite(-2, -2, 0.4, 0.4, 0.5, 0, 1.0, 0.5);
 }
 
 void shootMissle() {
-  if(missle1Active == 0) {
-    missle1Active = 1;
-    shootMissleSprite(missle1);
+  int i;
+
+  for(i = 0; i < 1; i += 1) {
+    if(get(misslesActive, i) == 0) {
+      shootMissleSprite(get(missles, i));
+    }
   }
 }
 
 void init() {
   // make sprites ahead of time
   core = newSprite(0.5, 0.5, 1, 1, 0, 0, 0.5, 0.5);
-  missle1 = newSprite(-10, -10, 0.4, 0.4, 0.5, 0, 1.0, 0.5);
 }
 
 void loop() {
-  if(missle1Active == 1) {
-    missle1->x += 0.1;
-    missle1->y += 0.1;
+  int i;
+
+  for(i = 0; i < 1; i += 1) {
+    if(get(misslesActive, i) == 1) {
+      structptr(Sprite) missle = get(missles, i);
+      missle->x += 0.1;
+      missle->y += 0.1;
+    }
   }
 
   timeout -= 1;
